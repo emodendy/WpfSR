7cd56136764d3059f13aca095aab8fa67eebefd9
emodendy <arsenij.cherenkov.2017@mail.ru>
2024-04-08 19:09:54 +0300

    sdgfsdfsd

--- WpfSR/MainWindow.xaml.cs
+++ WpfSR/MainWindow.xaml.cs
@@ -28,7 +28,8 @@ namespace WpfSR
                 "Последовательное", "Параллельное"
             };
         }
-        
+        double first_rez;
+        double second_rez;
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (type.SelectedItem == "Последовательное")
@@ -45,7 +46,63 @@ namespace WpfSR
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            first
+            first.Text = null;
+            second.Text = null;
+            result.Content = "Ваш результат";
         }
+
+        private void first_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                if (Convert.ToDouble(first.Text) != 0 || Convert.ToDouble(first.Text) >= 0)
+                {
+                    first_rez = Convert.ToInt32(first.Text);
+                }
+                else
+                {
+                    MessageBox.Show("Введите число больше 0!!");
+                }
+            }
+            catch (Exception ex)
+            {
+                first.Text = "";
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (type.SelectedItem == "Последовательное")
+            {
+                result.Content = $"Ваш результат {first_rez + second_rez}";
+            }
+            else if (type.SelectedItem == "Параллельное")
+            {
+                result.Content = $"Ваш результат {first_rez * second_rez/(first_rez + second_rez)}";
+            }else
+            {
+                MessageBox.Show("Выберите тип соединения!");
+            }
+        }
+
+        private void second_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                if (Convert.ToDouble(second.Text) != 0 || Convert.ToDouble(second.Text) >= 0)
+                {
+                    second_rez = Convert.ToInt32(second.Text);
+                }
+                else
+                {
+                    MessageBox.Show("Введите число больше 0!!");
+                }
+            }
+            catch (Exception ex)
+            {
+                second.Text = "";
+            }
+        }   
+ 
     }
 }
